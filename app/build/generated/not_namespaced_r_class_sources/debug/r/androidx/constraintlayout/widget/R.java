790b8b1a3fcaf20907603def7a316b2d9a15525a
Shraman-jain <jainshraman1998@gmail.com>
2020-02-12 22:11:27 +0530

    change2

--- app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f08002d;
-        public static final int end = 0x7f080062;
-        public static final int gone = 0x7f080071;
-        public static final int invisible = 0x7f08007c;
-        public static final int left = 0x7f080081;
-        public static final int packed = 0x7f080096;
-        public static final int parent = 0x7f080098;
-        public static final int percent = 0x7f08009b;
-        public static final int right = 0x7f0800a9;
-        public static final int spread = 0x7f0800cc;
-        public static final int spread_inside = 0x7f0800cd;
-        public static final int start = 0x7f0800d2;
-        public static final int top = 0x7f0800e7;
-        public static final int wrap = 0x7f0800fb;
+        public static final int end = 0x7f080063;
+        public static final int gone = 0x7f080072;
+        public static final int invisible = 0x7f08007d;
+        public static final int left = 0x7f080082;
+        public static final int packed = 0x7f080098;
+        public static final int parent = 0x7f08009a;
+        public static final int percent = 0x7f08009d;
+        public static final int right = 0x7f0800ab;
+        public static final int spread = 0x7f0800ce;
+        public static final int spread_inside = 0x7f0800cf;
+        public static final int start = 0x7f0800d4;
+        public static final int top = 0x7f0800e9;
+        public static final int wrap = 0x7f0800fd;
     }
     public static final class styleable {
         private styleable() {}
--- app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/transition/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/transition/R.java
@@ -85,37 +85,37 @@ public final class R {
         public static final int actions = 0x7f080022;
         public static final int async = 0x7f080028;
         public static final int blocking = 0x7f08002c;
-        public static final int chronometer = 0x7f080041;
-        public static final int forever = 0x7f08006f;
-        public static final int ghost_view = 0x7f080070;
-        public static final int icon = 0x7f080076;
-        public static final int icon_group = 0x7f080077;
-        public static final int info = 0x7f08007b;
-        public static final int italic = 0x7f08007d;
-        public static final int line1 = 0x7f080082;
-        public static final int line3 = 0x7f080083;
-        public static final int normal = 0x7f080091;
-        public static final int notification_background = 0x7f080092;
-        public static final int notification_main_column = 0x7f080093;
-        public static final int notification_main_column_container = 0x7f080094;
-        public static final int parent_matrix = 0x7f08009a;
-        public static final int right_icon = 0x7f0800aa;
-        public static final int right_side = 0x7f0800ab;
-        public static final int save_image_matrix = 0x7f0800ac;
-        public static final int save_non_transition_alpha = 0x7f0800ad;
-        public static final int save_scale_type = 0x7f0800ae;
-        public static final int tag_transition_group = 0x7f0800d7;
-        public static final int tag_unhandled_key_event_manager = 0x7f0800d8;
-        public static final int tag_unhandled_key_listeners = 0x7f0800d9;
-        public static final int text = 0x7f0800da;
-        public static final int text2 = 0x7f0800db;
-        public static final int time = 0x7f0800e3;
-        public static final int title = 0x7f0800e4;
-        public static final int transition_current_scene = 0x7f0800ea;
-        public static final int transition_layout_save = 0x7f0800eb;
-        public static final int transition_position = 0x7f0800ec;
-        public static final int transition_scene_layoutid_cache = 0x7f0800ed;
-        public static final int transition_transform = 0x7f0800ee;
+        public static final int chronometer = 0x7f080042;
+        public static final int forever = 0x7f080070;
+        public static final int ghost_view = 0x7f080071;
+        public static final int icon = 0x7f080077;
+        public static final int icon_group = 0x7f080078;
+        public static final int info = 0x7f08007c;
+        public static final int italic = 0x7f08007e;
+        public static final int line1 = 0x7f080083;
+        public static final int line3 = 0x7f080084;
+        public static final int normal = 0x7f080093;
+        public static final int notification_background = 0x7f080094;
+        public static final int notification_main_column = 0x7f080095;
+        public static final int notification_main_column_container = 0x7f080096;
+        public static final int parent_matrix = 0x7f08009c;
+        public static final int right_icon = 0x7f0800ac;
+        public static final int right_side = 0x7f0800ad;
+        public static final int save_image_matrix = 0x7f0800ae;
+        public static final int save_non_transition_alpha = 0x7f0800af;
+        public static final int save_scale_type = 0x7f0800b0;
+        public static final int tag_transition_group = 0x7f0800d9;
+        public static final int tag_unhandled_key_event_manager = 0x7f0800da;
+        public static final int tag_unhandled_key_listeners = 0x7f0800db;
+        public static final int text = 0x7f0800dc;
+        public static final int text2 = 0x7f0800dd;
+        public static final int time = 0x7f0800e5;
+        public static final int title = 0x7f0800e6;
+        public static final int transition_current_scene = 0x7f0800ec;
+        public static final int transition_layout_save = 0x7f0800ed;
+        public static final int transition_position = 0x7f0800ee;
+        public static final int transition_scene_layoutid_cache = 0x7f0800ef;
+        public static final int transition_transform = 0x7f0800f0;
     }
     public static final class integer {
         private integer() {}
@@ -135,7 +135,7 @@ public final class R {
     public static final class string {
         private string() {}
 
-        public static final int status_bar_notification_info_overflow = 0x7f0d004f;
+        public static final int status_bar_notification_info_overflow = 0x7f0d0050;
     }
     public static final class style {
         private style() {}
